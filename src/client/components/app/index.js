c04a2716b5914806144c6c5ab28bbae600ffe981
Rvor <nhamtybv@hotmail.com>
2016-04-29 00:50:48 +0700

    Init version of react isomorphic application

---
+++ src/client/components/app/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {} from './style.less';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello world from React components & Rvor</div>
+    );
+  }
+}
+
+export default App;
